574e4c97dff7e200cf2967e3be1ce295b9358d17
dariofusco <fdario@hotmail.it>
2023-07-07 17:25:55 +0200

    fix visualizzazione numeri in html

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,24 +2,25 @@
 Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l’altro di verde. 
 Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari. */
 
+// Creo un array vuoto.
 const numbersArray = []
 
-
+// Genero numeri casuali da inserire nell'array
 for (i = 0; i < 10; i++) {
     const randomNumber = Math.floor(Math.random() * 50);
     numbersArray.push(randomNumber);
 }
-
+// Seleziono i numeri pari e dispari e li stampo nei div creati in html
 for (i = 0; i < numbersArray.length; i++) {
     const numberArray = numbersArray[i]
     console.log(numberArray)
     if (numberArray % 2 == 0) {
         console.log("il numero è pari")
-        document.getElementById("text-green").innerHTML += numberArray
+        document.getElementById("text-green").innerHTML += `<div>${numberArray}<div>`
         document.getElementById("text-green").className = "text-success"
     } else {
         console.log("il numero è dispari")
-        document.getElementById("text-red").innerHTML += numberArray
+        document.getElementById("text-red").innerHTML += `<div>${numberArray}<div>`
         document.getElementById("text-red").className = "text-danger"
     }
 }
